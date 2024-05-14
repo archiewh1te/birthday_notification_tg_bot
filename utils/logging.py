0a63a26d195db428802ed8acc6ab8a43b275fe90
ArchieWh1te <a.kilchenko@262233.ru>
2024-05-14 15:46:32 +0900

    update

--- utils/logging.py
+++ utils/logging.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import os  # Импортируем модуль os
 from logging.handlers import TimedRotatingFileHandler
 
 format_msg = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
@@ -7,8 +8,15 @@ format_msg = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Путь к файлу лога
+log_path = f'logs/server/logs_info {datetime.datetime.now().strftime("%Y-%m-%d")}.log'
+
+# Проверяем, существует ли директория. Если нет, создаем ее.
+if not os.path.exists(os.path.dirname(log_path)):
+    os.makedirs(os.path.dirname(log_path))
+
 file_handler = logging.handlers.TimedRotatingFileHandler(
-    f'logs/server/logs_info {datetime.datetime.now().strftime("%Y-%m-%d")}.log',
+    log_path,
     when='midnight',
     interval=1,
     encoding='utf-8')
